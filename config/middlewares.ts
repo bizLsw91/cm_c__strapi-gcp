82f367e152c566d6c6a2347ade44fa86911e8f70
bizLsw91 <biz.lsw91@gmail.com>
2026-03-16 21:55:32 +0900

    fix(config): increase upload limit to 50MB in middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -40,7 +40,17 @@ export default [
   },
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '50mb',
+      jsonLimit: '50mb',
+      textLimit: '50mb',
+      formidable: {
+        maxFileSize: 50 * 1024 * 1024, // 50MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
